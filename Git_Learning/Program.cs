c918a51c9a49ae414e62498315a69347031dfa16
D4R102004 <161531325+D4R102004@users.noreply.github.com>
2024-12-04 02:27:34 -0500

    first learning commit

---
+++ Git_Learning/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] srgs)
+    {
+        System.Console.WriteLine("polo");
+    }
+}
+
+    class GitLearn
+{
+    public GitLearn(int daysLearning)
+    {
+        DaysLearning = daysLearning;
+    }
+
+    public int DaysLearning { get; }
+}
